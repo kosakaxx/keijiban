994114efc4f5d06fb5b7416245a5c120933f4033
Oue <Oue@corin>
2023-07-28 11:15:13 +0900

    リクエストのnullを解決 @autwiredがnullになる

--- backEnd/src/main/java/jp/co/netboard/controller/InsertPostController.java
+++ backEnd/src/main/java/jp/co/netboard/controller/InsertPostController.java
@@ -3,6 +3,7 @@ package jp.co.netboard.controller;
 import jp.co.netboard.request.InsertPostRequest;
 import jp.co.netboard.service.InsertPostService;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,7 +15,7 @@ public class InsertPostController {
     @PostMapping(value = "/post/insert",
             produces = {"application/json"},
             consumes = {"application/json"})
-    public String insertPost (InsertPostRequest request) throws Exception{
+    public String insertPost (@RequestBody InsertPostRequest request) throws Exception{
         InsertPostService  insertPostService = new InsertPostService();
         return insertPostService.insertPost(request);
     }
