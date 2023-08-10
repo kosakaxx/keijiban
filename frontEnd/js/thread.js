c01a3dc786b46ab64263b39554fe58399073ff3c
toma-suzuki <toma.szk1005@gmail.com>
2023-08-10 17:22:50 +0900

    Fix: insert繋ぎ込んだよぉ〜〜ん

--- frontEnd/js/thread.js
+++ frontEnd/js/thread.js
@@ -1,13 +1,32 @@
-const getList = async() => {
-  const response = await fetch('http://localhost:8080/post/list', {method: 'GET'})
-    .then((res) => res.json())
-    .catch((err) => {
-      alert(`一覧取得に失敗しました。エラー：${err}`);
+const getList = async () => {
+    const response = await fetch('http://localhost:8080/post/list', {method: 'GET'})
+        .then((res) => res.json())
+        .catch((err) => {
+            alert(`一覧取得に失敗しました。エラー：${err}`);
+        })
+
+    if (response.code === "200") {
+        const list = response.list;
+
+
+    }
+}
+
+const sendPost = async () => {
+    const author = document.getElementById('author_name').value
+    const message = document.getElementById('post_message').value
+    const pass = document.getElementById('password').value
+
+    const requestBody = {
+        authorName: author,
+        postMessage: message,
+        password: pass
+    }
+
+    const res = await fetch('http://localhost:8080/post/insert', {
+        method: 'POST',
+        body: JSON.stringify(requestBody)
     })
 
-  if (response.code === "200") {
-    const list = response.list;
-
-
-  }
+    console.log(res)
 }
