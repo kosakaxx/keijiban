69b9bba0313b8d01bd59c01a0c11ab9f5453ec48
Oue <Oue@corin>
2023-08-18 09:49:15 +0900

    streamにしてみた

--- backEnd/src/main/java/jp/co/netboard/service/GetPostsService.java
+++ backEnd/src/main/java/jp/co/netboard/service/GetPostsService.java
@@ -7,10 +7,12 @@ import jp.co.netboard.response.Post;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class GetPostsService {
@@ -25,19 +27,26 @@ public class GetPostsService {
      */
     public GetPostsResponse getPosts() {
 
-        List<Post> postList = new ArrayList<Post>();
 
         //DBから全ての記事を検索
-        List<KeijibanEntity> postsEntityList = keijibanRepository.findAll();
-        postsEntityList.forEach(entity -> {
+        List<KeijibanEntity> postEntityList = keijibanRepository.findAll();
+
+        //投稿日時が存在しないデータは除外する
+        List<KeijibanEntity> filteredPostEntityList = postEntityList.stream().filter(p ->
+                !StringUtils.isEmpty(p.getCreatedDatetime())
+        ).collect(Collectors.toList());
+
+        List<Post> postList = new ArrayList<Post>();
+        filteredPostEntityList.forEach(entity -> {
             Post post = new Post();
             BeanUtils.copyProperties(entity, post);
-            if (post.getCreatedDatetime() != null) {
-                postList.add(post);
-            }
+            postList.add(post);
         });
+
         //投稿日時を降順にしてソート
         postList.sort(Comparator.comparing(Post::getCreatedDatetime).reversed());
+
+        //投稿数の算出
         Integer postCount = postList.size();
 
         //レスポンスにセット
